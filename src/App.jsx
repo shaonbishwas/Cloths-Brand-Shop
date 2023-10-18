58105414459dcf19d1489c3b3288e3db0fa4baf8
shaonbishwas <shaonbishwas01@gmail.com>
2023-10-18 21:26:44 +0800

    firebase deploy check

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
-
-import './App.css'
-import Footer from './components/Footer/Footer'
+import { useEffect, useState } from "react";
+import "./App.css";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-
+  const [products, setProducts] = useState();
+  useEffect(() => {
+    fetch(
+      "https://fashion-and-apparel-server-a4t92rbl6-shaon-bishwas-projects.vercel.app/products"
+    )
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
   return (
     <>
-      
       <h1>Vite + React</h1>
+      {products?.map((p) => (
+        <div key={p._id}>
+          <h1>{p.name}</h1>
+          <h1>{p.brand_name}</h1>
+          <h1>{p._id}</h1>
+        </div>
+      ))}
       <Footer></Footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAf0w-7_GKmWw9fcTYjm92ugYkhZxaxQc0",
+  authDomain: "fashion-and-apparel-2fcde.firebaseapp.com",
+  projectId: "fashion-and-apparel-2fcde",
+  storageBucket: "fashion-and-apparel-2fcde.appspot.com",
+  messagingSenderId: "578953638047",
+  appId: "1:578953638047:web:25cabcaea99a65d50bf2aa"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
